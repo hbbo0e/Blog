820a0b3271e6c87df02f6b6fa5a37a4dc9161dfd
hbbo0e <44goodluck44@gmail.com>
2024-12-09 17:16:13 +0900

    test: issue #5 전체 목록 조회를 위한 controller, service 변경 및 dto 생성

--- src/main/java/com/example/blog/controller/BlogController.java
+++ src/main/java/com/example/blog/controller/BlogController.java
@@ -2,11 +2,14 @@ package com.example.blog.controller;
 
 import com.example.blog.domain.Article;
 import com.example.blog.dto.AddArticleRequest;
+import com.example.blog.dto.ArticleResponse;
 import com.example.blog.repository.BlogRepository;
 import com.example.blog.service.BlogService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,5 +29,16 @@ public class BlogController {
 
     return ResponseEntity.status(HttpStatus.CREATED)
         .body(savedArticle);
+  }
+
+  @GetMapping("/articles")
+  public ResponseEntity<List<ArticleResponse>> findAllArticles(){
+    List<ArticleResponse> articles = blogService.findAll()
+        .stream()
+        .map(ArticleResponse::new)
+        .toList();
+
+    return ResponseEntity.ok()
+        .body(articles);
   }
 }
--- src/main/java/com/example/blog/service/BlogService.java
+++ src/main/java/com/example/blog/service/BlogService.java
@@ -3,6 +3,7 @@ package com.example.blog.service;
 import com.example.blog.domain.Article;
 import com.example.blog.dto.AddArticleRequest;
 import com.example.blog.repository.BlogRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,4 +15,8 @@ public class BlogService {
   public Article save(AddArticleRequest request){
     return blogRepository.save(request.toEntity());
   }
+  public List<Article> findAll(){
+    return blogRepository.findAll();
+  }
+
 }
