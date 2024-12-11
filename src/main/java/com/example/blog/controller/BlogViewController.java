0bdae1f23a0917b6a1a41dd669932c886247de17
hbbo0e <44goodluck44@gmail.com>
2024-12-11 11:17:51 +0900

    feat: issue #11 수정 구현

--- src/main/java/com/example/blog/controller/BlogViewController.java
+++ src/main/java/com/example/blog/controller/BlogViewController.java
@@ -5,12 +5,12 @@ import com.example.blog.dto.ArticleListViewResponse;
 import com.example.blog.dto.ArticleViewResponse;
 import com.example.blog.service.BlogService;
 import java.util.List;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
 @Controller
@@ -34,5 +34,17 @@ public class BlogViewController {
     model.addAttribute("article", new ArticleViewResponse(article));
 
     return "article";
+  }
+
+  @GetMapping("/new-article")
+  public String newArticle(@RequestParam(required = false) Long id, Model model){
+    if(id==null){
+      model.addAttribute("article", new ArticleViewResponse());
+    } else{
+      Article article = blogService.findById(id);
+      model.addAttribute("article", new ArticleViewResponse(article));
+    }
+
+    return "newArticle";
   }
 }
--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -12,3 +12,27 @@ if (deleteButton){
     });
   });
 }
+
+const modifyButton = document.getElementById('modify-btn');
+
+if(modifyButton){
+  modifyButton.addEventListener('click', event => {
+    let params = new URLSearchParams(location.search);
+    let id = params.get('id');
+
+    fetch(`/api/article/${id}`, {
+      method: 'PUT',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: document.getElementById('title').value,
+        content: document.getElementById('content').value
+      })
+    })
+    .then(() => {
+      alert('completed modify');
+      location.replace(`/article/${id}`);
+    });
+  });
+}
