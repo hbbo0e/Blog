a3e9533325682962d627aa39e960ba12999f39cf
hbbo0e <44goodluck44@gmail.com>
2025-01-09 21:39:56 +0900

    feat: issue #16

--- src/main/java/com/example/blog/config/WebSecurityConfig.java
+++ src/main/java/com/example/blog/config/WebSecurityConfig.java
@@ -1,67 +1,67 @@
-package com.example.blog.config;
-
-import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
-
-import com.example.blog.service.UserDetailService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-
-
-/**
- * oauth2 와 jwt 를 함께 사용하려면 기존 스프링 시큐리티를 구현하며 작성한 설정이 아니라 다른 설정을 사용해야 함
- * oauth2, jwt 에 맞게 설정 파일 수정 위하여 주석 처리 -> WebOAuthSecurityConfig 작성
- */
-@RequiredArgsConstructor
-@Configuration
-public class WebSecurityConfig {
-
-  private final UserDetailService userService;
-
-  @Bean
-  public WebSecurityCustomizer configure(){
-    return (web) -> web.ignoring()
-        .requestMatchers(toH2Console())
-        .requestMatchers("/static/**");
-  }
-
-  @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-    return http
-        .authorizeRequests()
-        .requestMatchers("/login", "/signup", "/user").permitAll()
-        .anyRequest().authenticated()
-        .and()
-        .formLogin()
-        .loginPage("/login")
-        .defaultSuccessUrl("/articles")
-        .and()
-        .logout()
-        .logoutSuccessUrl("/login")
-        .invalidateHttpSession(true)
-        .and()
-        .csrf().disable()
-        .build();
-  }
-
-  @Bean
-  public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailService userDetailService) throws Exception{
-    return http
-        .getSharedObject(AuthenticationManagerBuilder.class)
-        .userDetailsService(userService)
-        .passwordEncoder(bCryptPasswordEncoder)
-        .and()
-        .build();
-  }
-
-  @Bean
-  public BCryptPasswordEncoder bCryptPasswordEncoder(){
-    return new BCryptPasswordEncoder();
-  }
-}
+//package com.example.blog.config;
+//
+//import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+//
+//import com.example.blog.service.UserDetailService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//
+///**
+// * oauth2 와 jwt 를 함께 사용하려면 기존 스프링 시큐리티를 구현하며 작성한 설정이 아니라 다른 설정을 사용해야 함
+// * oauth2, jwt 에 맞게 설정 파일 수정 위하여 주석 처리 -> WebOAuthSecurityConfig 작성
+// */
+//@RequiredArgsConstructor
+//@Configuration
+//public class WebSecurityConfig {
+//
+//  private final UserDetailService userService;
+//
+//  @Bean
+//  public WebSecurityCustomizer configure(){
+//    return (web) -> web.ignoring()
+//        .requestMatchers(toH2Console())
+//        .requestMatchers("/static/**");
+//  }
+//
+//  @Bean
+//  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+//    return http
+//        .authorizeRequests()
+//        .requestMatchers("/login", "/signup", "/user").permitAll()
+//        .anyRequest().authenticated()
+//        .and()
+//        .formLogin()
+//        .loginPage("/login")
+//        .defaultSuccessUrl("/articles")
+//        .and()
+//        .logout()
+//        .logoutSuccessUrl("/login")
+//        .invalidateHttpSession(true)
+//        .and()
+//        .csrf().disable()
+//        .build();
+//  }
+//
+//  @Bean
+//  public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailService userDetailService) throws Exception{
+//    return http
+//        .getSharedObject(AuthenticationManagerBuilder.class)
+//        .userDetailsService(userService)
+//        .passwordEncoder(bCryptPasswordEncoder)
+//        .and()
+//        .build();
+//  }
+//
+//  @Bean
+//  public BCryptPasswordEncoder bCryptPasswordEncoder(){
+//    return new BCryptPasswordEncoder();
+//  }
+//}
--- src/main/java/com/example/blog/controller/BlogViewController.java
+++ src/main/java/com/example/blog/controller/BlogViewController.java
@@ -16,35 +16,37 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class BlogViewController {
 
-  private final BlogService blogService;
+    private final BlogService blogService;
 
-  @GetMapping("/articles")
-  public String getArticles(Model model){
-    List<ArticleListViewResponse> articles = blogService.findAll().stream()
-        .map(ArticleListViewResponse::new)
-        .toList();
-    model.addAttribute("articles", articles); // 블로그 글 리스트 저장
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll()
+            .stream()
+            .map(ArticleListViewResponse::new)
+            .toList();
+        model.addAttribute("articles", articles);
 
-    return "articleList"; // articleList.html 이라는 뷰 조회
-  }
-
-  @GetMapping("/article/{id}")
-  public String getArticle(@PathVariable Long id, Model model){
-    Article article = blogService.findById(id);
-    model.addAttribute("article", new ArticleViewResponse(article));
-
-    return "article";
-  }
-
-  @GetMapping("/new-article")
-  public String newArticle(@RequestParam(required = false) Long id, Model model){
-    if(id==null){
-      model.addAttribute("article", new ArticleViewResponse());
-    } else{
-      Article article = blogService.findById(id);
-      model.addAttribute("article", new ArticleViewResponse(article));
+        return "articleList";
     }
 
-    return "newArticle";
-  }
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
+    }
+
+
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        if (id == null) {
+            model.addAttribute("article", new ArticleViewResponse());
+        } else {
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+
+        return "newArticle";
+    }
 }
