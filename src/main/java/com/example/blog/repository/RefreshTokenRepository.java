ff11157d91dc9d6493524d21dfc853b5f8285045
hbbo0e <44goodluck44@gmail.com>
2025-01-09 11:45:06 +0900

    feat: issue #15 refresh token domain 구현

---
+++ src/main/java/com/example/blog/repository/RefreshTokenRepository.java
@@ -0,0 +1,10 @@
+package com.example.blog.repository;
+
+import com.example.blog.domain.RefreshToken;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(Long userId);
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
